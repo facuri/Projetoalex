47c912eaeb1fe10edbac760ef178220de96e03bb
dalmo <mffacuri@terra.com.br>
2023-01-29 16:03:21 -0200

    Adicionando campo data Nascimento2 na classe Pessoa

--- ProjetoAlexRevisao/src/curso/java/alex/Pessoa.java
+++ ProjetoAlexRevisao/src/curso/java/alex/Pessoa.java
@@ -7,6 +7,7 @@ public class Pessoa {
 	private String sobrenome;
 	private Integer idade;
 	private Date dataNascimento;
+	private Date dataNascimento2;
 
 	public String getNome() {
 		return nome;
